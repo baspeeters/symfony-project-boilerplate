873cac554de20b54b58f90a745bcf96cd370124f
Bas Peeters <bas@peete.rs>
2018-11-10 09:12:06 +0800

    Fix bugs found by Scrutinizer

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Boilerplate for a Symfony project.
 
 [![Build Status](https://travis-ci.org/baspeeters/symfony-project-boilerplate.svg?branch=master)](https://travis-ci.org/baspeeters/symfony-project-boilerplate)
 [![Coverage Status](https://coveralls.io/repos/github/baspeeters/symfony-project-boilerplate/badge.svg?branch=master)](https://coveralls.io/github/baspeeters/symfony-project-boilerplate?branch=master)
-
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/baspeeters/symfony-project-boilerplate/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/baspeeters/symfony-project-boilerplate/?branch=master)
 
 ## Test
 
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,12 @@
 
 namespace App;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\Config\Resource\FileResource;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
 
@@ -26,12 +28,21 @@ class Kernel extends BaseKernel
         return $this->getProjectDir().'/var/log';
     }
 
+
+    /**
+     * @codeCoverageIgnore
+     * @throws Exception
+     */
     public function registerBundles()
     {
         $contents = require $this->getProjectDir().'/config/bundles.php';
         foreach ($contents as $class => $envs) {
             if (isset($envs['all']) || isset($envs[$this->environment])) {
-                yield new $class();
+                $newClass = new $class();
+
+                if ($newClass instanceof BundleInterface) {
+                    yield $newClass;
+                }
             }
         }
     }
